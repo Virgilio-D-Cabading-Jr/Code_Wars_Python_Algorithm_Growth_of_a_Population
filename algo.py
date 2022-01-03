3a06d02cda390f6e7e4729e9558b599e8826739c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-02 22:30:54 -0600

    get while loop to iterate through the number of years

--- algo.py
+++ algo.py
@@ -1,3 +1,10 @@
 ###################################################
 #   Code Wars: Growth of a Population
 ###################################################
+
+def nb_year(p0, percent, aug, p):
+    yearCount = 0;
+    pop = p0;
+    while (pop < p) {
+        
+    } 
