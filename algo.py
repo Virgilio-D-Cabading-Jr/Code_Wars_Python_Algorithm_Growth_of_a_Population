6667e5ea4950e14ac0f94ea5e4104e3acc06a319
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-02 22:42:37 -0600

    make sure population is an integer

--- algo.py
+++ algo.py
@@ -5,6 +5,10 @@
 def nb_year(p0, percent, aug, p):
     yearCount = 0;
     pop = p0;
-    while (pop < p) {
-        
-    } 
+    while (pop < p) :
+        pop += int(pop * (percent / 100)) + aug;
+        yearCount += 1;
+        print ("Year:", yearCount, " | Population:", pop);
+    return pop;
+    
+print ("", nb_year(1000, 2, 50, 1200));
