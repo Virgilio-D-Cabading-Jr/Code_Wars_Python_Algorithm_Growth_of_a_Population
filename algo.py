79fe078072bd801cd68d2725ad3911d58a7eb61c
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2022-01-02 22:45:14 -0600

    remote debug prints

--- algo.py
+++ algo.py
@@ -8,7 +8,6 @@ def nb_year(p0, percent, aug, p):
     while (pop < p) :
         pop += int(pop * (percent / 100)) + aug;
         yearCount += 1;
-        print ("Year:", yearCount, " | Population:", pop);
     return yearCount;
 
 print ("", nb_year(1000, 2, 50, 1200));
